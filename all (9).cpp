2273eb1dc4d4277686fa751d4f79da7e03c0d5af
Divyansh-kumar-git <kumardivyansh4747@gmail.com>
2025-12-23 16:28:39 +0530

    Add files via upload

---
+++ all (9).cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+int x;
+cin>>x;
+switch(x)
+{
+case 1:
+cout<<"monday";
+break;
+
+case 2:
+cout<<"tuesday";
+
+case 3:
+cout<<"wednesday";
+break;
+
+case 4:
+cout<<"thursday";
+break;
+
+case 5:
+cout<<"friday";
+break;
+
+case 6:
+cout<<"saturday";
+break;
+
+case 7:
+cout<<"sunday";
+break;
+
+default:
+cout<<"Wrong value";
+};
+}
