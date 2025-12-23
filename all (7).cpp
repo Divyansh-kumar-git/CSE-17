b9ceebe05a42c81a6dac75d18ea4d5fe4378ebdb
Divyansh-kumar-git <kumardivyansh4747@gmail.com>
2025-12-23 16:28:25 +0530

    Add files via upload

---
+++ all (7).cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int y;
+    cout << "Enter the value of y: ";
+    cin >> y;
+
+    int i = 2; 
+    while (i <= y) {
+        cout << i << " ";
+        i=i+2; 
+    }
+
+    return 0;
+
+
+}
